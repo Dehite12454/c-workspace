c2c8f829887c4dad8268ed4bf00309dcde12a42b
dehite12454 <donghwi3330@gmail.com>
2025-01-19 11:21:36 +0000

    1

---
+++ 2025-01-19-value.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int a = 3;
+    float b = 3.141592f;
+    printf("a : %.2f\n", b); //소수점 둘째 자리로 반올림 하여라
+    printf("b : %6.3f",b); //숫자의 자리수를 6자리로 맞추고 소수점 셋째 자리로 반올림 하여라
+}
