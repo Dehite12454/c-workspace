8ca0c6ab15a0cfdeff0430cfae9b6a93a8ed57ce
dehite12454 <donghwi3330@gmail.com>
2025-01-21 11:34:34 +0000

    2

--- 2025-01-21-prob3.c
+++ 2025-01-21-prob3.c
@@ -3,6 +3,36 @@
 #include <stdio.h>
 int main(){
     int arr[5] = {90,40,20,100,80};
+    int arr1[10][5];
+    int a;
+    int b;
+    int score;
+    int i = 0;
+
+    for (b = 0;b<5;b++){
+        score = arr[b]/10;
+        for (a = 0;a<10;a++){
+            if (10-score > 0){
+                arr1[a][b] = 0;
+            }
+            else {
+                arr1[a][b] = 1;
+            }
+            score++;
+        }
+    }
+
+    for (a = 0;a<10;a++){
+        for (b = 0;b<5;b++){
+            if (arr1[a][b] == 0){
+                printf(" ");
+            }
+            else {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
     
 
     return 0;
