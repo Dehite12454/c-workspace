eeb44580858881ea9a2654f82f506a9bc8f15357
dehite12454 <donghwi3330@gmail.com>
2025-01-25 11:15:18 +0000

    2

--- 2025-01-23.c
+++ 2025-01-23.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 
+int add_number(int (*parr)[3]);
 int main() {
-  int arr[3] = {1, 2, 3};
-  int (*parr)[3] = &arr;    // arr은 int*형 포인터, &arr은 int*[3]형 포인터
+  int arr[2][3] = {{2,3,3},{2,3,4}};
 
-  printf("arr[1] : %d \n", arr[1]);
-  printf("parr[1] : %d \n", (*parr)[1]);
+  add_number(arr);
+
+  printf("배열의 각 원소 : %d, %d, %d", arr[0][0], arr[0][1], arr[0][2]);
+
+  return 0;
+}
+
+int add_number(int (*parr)[3]) {
+  int (*arr1)[3];
+  int i;
+  arr1 = parr;
+  for (i = 0; i < 3; i++) {
+    arr1[0][i] += 1;
+  }
+  return 0;
 }
