d3529e9dc1daf5f6d517f981cc71737e5c0579a5
dehite12454 <donghwi3330@gmail.com>
2025-02-07 11:07:15 +0000

    2

---
+++ 2025-02-07-prob1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+int mkarray(int *parr,int size);
+double avg(int arr[5],int size);
+int compare(double a,double b);
+int main(){
+    int std1[5],std2[5],std3[5],std4[5],std5[5];
+    double tavg;
+
+    mkarray(std1,5);
+    mkarray(std2,5);
+    mkarray(std3,5);
+    mkarray(std4,5);
+    mkarray(std5,5);
+
+    tavg = (avg(std1,5)+avg(std2,5)+avg(std3,5)+avg(std4,5)+avg(std5,5))/5;
+
+    compare(avg(std1,5),tavg);
+    compare(avg(std2,5),tavg);
+    compare(avg(std3,5),tavg);
+    compare(avg(std4,5),tavg);
+    compare(avg(std5,5),tavg);
+
+    return 0;
+}
+
+int mkarray(int *parr,int size){
+    int a;
+    for (a = 0;a<size;a++){
+        scanf("%d",&parr[a]);
+    }
+    return 0;
+}
+
+double avg(int arr[5],int size){
+    double avg;
+    int sum = 0;
+    int a;
+    for (a = 0;a<size;a++){
+        sum += arr[a];
+    }
+    avg = sum/size;
+    return avg;
+}
+
+int compare(double a,double b){
+    if (a>b){
+        printf("합격\n");
+    }
+    else{
+        printf("불합격\n");
+    }
+    return 0;
+}
