bc6ade5319c27f37834e5564532ddf8a19bd055b
dehite12454 <donghwi3330@gmail.com>
2025-03-14 11:22:35 +0000

    2

--- 2025-03-10-debug.c
+++ 2025-03-10-debug.c
@@ -12,7 +12,7 @@ int main(){
     return 0;
 }
 
-int return_first(char *arr[][3][30]){
+int return_first(char *arr[][3][30]){   // 정석대로 *arr[][3][30]으로 포인터를 받아줘야지 프로그램 오류가 안난다.
     char s = arr[0][0][0];
 
     printf("%c",s);
